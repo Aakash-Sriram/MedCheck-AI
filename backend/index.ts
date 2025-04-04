4a878194ecf5a15bc247a51b8d3e5d797611300c
Aakash Sriram <194975502+Aakash-Sriram@users.noreply.github.com>
2025-04-04 16:42:53 +0530

    prismaClient

---
+++ backend/index.ts
@@ -0,0 +1,9 @@
+// index.ts
+import { PrismaClient } from '@prisma/client';
+
+const prismaClient = new PrismaClient();
+
+export default prismaClient;
+
+//import prismaClient from '../db';  // Import from db/index.ts
+
